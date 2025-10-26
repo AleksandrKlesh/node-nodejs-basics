20554cd86bd21eac625f21d04ec92d211361b432
AleksandrKlesh <alex73kl@gmail.com>
2025-10-26 22:46:24 +0700

    feat: add transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,17 @@
+import { Transform, pipeline } from 'node:stream';
+
 const transform = async () => {
-  // Write your code here
+  const reverseStream = new Transform({
+    transform(chunk, encoding, callback) {
+      const reversed = chunk.toString().split("").reverse().join('');
+      callback(null, reversed);
+    }
+  })
+  pipeline(process.stdin, reverseStream, process.stdout, (err) => {
+    if (err) {
+      console.error('FS operation failed', err);
+    }
+  });
 };
 
 await transform();
