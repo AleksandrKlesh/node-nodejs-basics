4d2ac09976d138d007b2ea87b1970069fe6de6df
AleksandrKlesh <alex73kl@gmail.com>
2025-10-26 16:07:53 +0700

    feat: add env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-  // Write your code here
+  const result = [];
+  for (let key in process.env) {
+    if (key.startsWith("RSS")) {
+      result.push(`${key}=${process.env[key]}`)
+    }
+  }
+  console.log(result.join("; "))
 };
 
 parseEnv();
